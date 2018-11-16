2db4362b66442117465ef3f44e4e2d89d83a9aa1
pramane2309 <praman2309@gmail.com>
2018-11-16 13:37:02 +0100

    import post

--- src/java/application/RestBank_API.java
+++ src/java/application/RestBank_API.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Produces;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.MediaType;
