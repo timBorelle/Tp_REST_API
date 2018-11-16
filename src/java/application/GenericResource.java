94651700e47251e2c092fe758640e0994c01c384
timBorelle <tim-borelle@protonmail.com>
2018-11-16 10:32:18 +0100

    First commit

---
+++ src/java/application/GenericResource.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@Path("generic")
+public class GenericResource {
+    
+    @GET
+    @Produces("text/plain")
+    public String getText() {
+        return "Mon premier service REST";
+    }
+}
